96af9b81e9f2e308f38cb21896643860a992dbcb
gudipatinrao <narasimharao.gudipati@ibm.com>
2020-09-14 15:03:25 -0500

    modifin network.tf to fix env name

--- network.tf
+++ network.tf
@@ -8,7 +8,6 @@ resource "ibm_is_subnet" "iac_app_subnet" {
   zone                     = var.vpc_zone_names[count.index]
   vpc                      = ibm_is_vpc.iac_app_vpc.id
   total_ipv4_address_count = 16
-  public_gateway           = ibm_is_public_gateway.pgw[count.index].id
   resource_group           = data.ibm_resource_group.group.id
 }
 
